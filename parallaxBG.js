e7fe210613ab8d58e262f82cc1f0fd3c71b8dd61
Travis Horn <horn.travis@gmail.com>
2012-04-06 08:05:17 -0500

    Removed script tags and formatted .js

--- parallaxBG.js
+++ parallaxBG.js
@@ -1,11 +1,12 @@
-<script>
   html = document.getElementsByTagName('html')[0];
   body = document.getElementsByTagName('body')[0];
+
   window.onscroll = function() {
-  if (body.scrollTop == 0) { var top = Math.round(html.scrollTop * -0.5); }
-  else { var top =  Math.round(body.scrollTop * -0.5); }
-  if (body.scrollLeft == 0) { var left = Math.round(html.scrollLeft * -0.5); }
-  else { var left = Math.round(body.scrollLeft * -0.5); }
-  body.style.backgroundPosition = left + "px " + top + "px";
+    if (body.scrollTop == 0) { var top = Math.round(html.scrollTop * -0.5); }
+    else { var top =  Math.round(body.scrollTop * -0.5); }
+    
+    if (body.scrollLeft == 0) { var left = Math.round(html.scrollLeft * -0.5); }
+    else { var left = Math.round(body.scrollLeft * -0.5); }
+
+    body.style.backgroundPosition = left + "px " + top + "px";
   };
-</script>
