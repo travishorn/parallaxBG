c13f1be92bbc4ee4be1b2b5e8fe6f569209ba604
Travis Horn <horn.travis@gmail.com>
2012-07-16 11:20:45 -0500

    Refactored code. Smaller filesize, fits in one anonymous function.

--- parallaxBG.js
+++ parallaxBG.js
@@ -1,12 +1,6 @@
-  html = document.getElementsByTagName('html')[0];
-  body = document.getElementsByTagName('body')[0];
-
-  window.onscroll = function() {
-    if (body.scrollTop == 0) { var top = Math.round(html.scrollTop * -0.5); }
-    else { var top =  Math.round(body.scrollTop * -0.5); }
-    
-    if (body.scrollLeft == 0) { var left = Math.round(html.scrollLeft * -0.5); }
-    else { var left = Math.round(body.scrollLeft * -0.5); }
-
-    body.style.backgroundPosition = left + "px " + top + "px";
-  };
+window.onscroll = function() {
+  var top  = (document.documentElement.scrollTop  || document.body.scrollTop)  * -0.5,
+      left = (document.documentElement.scrollLeft || document.body.scrollLeft) * -0.5;
+	
+	document.body.style.backgroundPosition = left + 'px ' + top + 'px';
+};
